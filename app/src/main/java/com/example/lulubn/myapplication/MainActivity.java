7f4d96e57ff2b0053ecd9eb06b62dcc43ebd8bc9
lulubn <lulubn1997@gmail.com>
2019-01-15 20:00:13 +0300

    last and push commit

--- app/src/main/java/com/example/lulubn/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lulubn/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
